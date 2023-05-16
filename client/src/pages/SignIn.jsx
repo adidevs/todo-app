06cf6f26bfc58ce74137e9a0f985351c0dec8723
Aditya Sharma <99022811+adidevs@users.noreply.github.com>
2023-05-16 15:52:36 +0530

    Remove logs

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -42,7 +42,6 @@ export default function SignIn() {
     }
 
     const submitInfo = (event) => {
-        console.log(baseURL);
         event.preventDefault();
         if (info.password !== "" && info.username !== "")
             checkLogin();
